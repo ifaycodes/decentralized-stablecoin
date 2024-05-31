120291f56dedab85c946f8e5209f57d66e444889
ifeoma <u.ifeoma22@gmail.com>
2024-05-31 10:35:59 +0100

    more tests

--- test/units/DSCEngineTest.t.sol
+++ test/units/DSCEngineTest.t.sol
@@ -8,25 +8,40 @@ import {DSCoin} from "../../src/DSCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import { ERC20Mock } from "../mocks/ERC20Mock.sol";
+import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
     DSCoin dscoin;
     DSCEngine dscengine;
     HelperConfig helperConfig;
+
     address ethUsdPriceFeed;
     address btcUsdPriceFeed;
     address weth;
+    address wbtc;
+    uint256 deployerKey;
 
     address public USER = makeAddr("USER");
     uint256 public constant AMOUNT_OF_COLLATERAL = 10 ether;
-    uint256 public constant STARTING_ERC20_BALANCE = 100 ether;
+    uint256 amountToMint = 100 ether;
+
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+    uint256 public constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 public constant LIQUIDATION_THRESHOLD = 50;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dscoin, dscengine, helperConfig) = deployer.run();
-        (ethUsdPriceFeed,,weth,,) = helperConfig.activeNetworkConfig();
+        (ethUsdPriceFeed,btcUsdPriceFeed,weth,wbtc,deployerKey) = helperConfig.activeNetworkConfig();
+
+        if (block.chainid == 31337) {
+            vm.deal(USER, STARTING_ERC20_BALANCE);
+        }
+            
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+        ERC20Mock(wbtc).mint(USER, STARTING_ERC20_BALANCE);
+    
     }
 
 // constructor
@@ -42,7 +57,7 @@ contract DSCEngineTest is Test {
         new DSCEngine(tokenAddresses, feedAddresses, address(dscoin));
     }
 
-// pricefeed test   
+    // pricefeed test   
     function testGetUsdValue() public view {
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
@@ -59,7 +74,7 @@ contract DSCEngineTest is Test {
         assertEq(amountWeth, expectedWeth);
     }
 
-//deposit tests
+    //deposit tests
     function testRevertsIfSendsZero() public {
 
         vm.startPrank(USER);
@@ -72,7 +87,7 @@ contract DSCEngineTest is Test {
     }
 
     function testRevertsWithUnapprovedCollateral() public {
-        ERC20Mock randToken = new ERC20Mock("RAN", "RAN", USER, AMOUNT_OF_COLLATERAL);
+        ERC20Mock randToken = new ERC20Mock("RAN", "RAN", USER, 100e18);
         vm.startPrank(USER);
         vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine__NotAllowedToken.selector, address(randToken)));
         dscengine.depositCollateral(address(randToken), AMOUNT_OF_COLLATERAL);
@@ -87,12 +102,75 @@ contract DSCEngineTest is Test {
         _;
     }
 
+    function testCanDepositCollateralWithoutMinting() public depositedCollateral {
+        uint256 userBalance = dscoin.balanceOf(USER);
+        assertEq(userBalance, 0);
+    }
+
     function testCanDepositedCollateralAndGetAccountInfo() public depositedCollateral {
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = dscengine.getAccountInformation(USER);
 
         uint256 expectedTotalDscMinted = 0;
         uint256 expectedDepositedAmount = dscengine.getTokenAmountFromUsd(weth, collateralValueInUsd);
+        console.log("totalDscMinted:", totalDscMinted);
+        console.log("collateralValueInUsd:", collateralValueInUsd);
+        console.log("expectedDepositedAmount:", expectedDepositedAmount);
+        console.log("AMOUNT_OF_COLLATERAL:", AMOUNT_OF_COLLATERAL);
+
         assertEq(totalDscMinted, expectedTotalDscMinted);
         assertEq(expectedDepositedAmount, AMOUNT_OF_COLLATERAL);
     } 
+
+    //depositCollateralAndMintDsc tests
+    function testRevertsIfMintedDscBreaksHealthFactor() public {
+        (, int256 price,,,) = MockV3Aggregator(ethUsdPriceFeed).latestRoundData();
+        amountToMint = (AMOUNT_OF_COLLATERAL * (uint256(price) * dscengine.getAdditionalFeedPrecision())) / dscengine.getPrecision();
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dscengine), AMOUNT_OF_COLLATERAL);
+
+        uint256 expectedHealthFactor =
+            dscengine.calculateHealthFactor(amountToMint, dscengine.getUsdValue(weth, AMOUNT_OF_COLLATERAL));
+        vm.expectRevert(abi.encodeWithSelector(DSCEngine.DSCEngine__BreakingHealthFactor.selector, expectedHealthFactor));
+        dscengine.depositCollateralAndMintDsc(weth, AMOUNT_OF_COLLATERAL, amountToMint);
+        vm.stopPrank();
+    }
+
+    modifier depositedCollateralAndMintedDsc() {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dscengine), AMOUNT_OF_COLLATERAL);
+        dscengine.depositCollateralAndMintDsc(weth, AMOUNT_OF_COLLATERAL, amountToMint);
+        vm.stopPrank();
+        _;
+    }
+
+    function testCanMintWithDepositedCollateral() public depositedCollateralAndMintedDsc {
+        uint256 userBalance = dscoin.balanceOf(USER);
+        assertEq(userBalance, amountToMint);
+    }
+
+    //burnDsc test
+    function testRevertsIfBurnAmountIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dscengine), AMOUNT_OF_COLLATERAL);
+        dscengine.depositCollateralAndMintDsc(weth, AMOUNT_OF_COLLATERAL, amountToMint);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dscengine.burnDsc(0);
+        vm.stopPrank();
+    }
+
+    function testCantBurnMoreThanUserHas() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        dscengine.burnDsc(1);
+    }
+
+    function testCanBurnDsc() public depositedCollateralAndMintedDsc {
+        vm.startPrank(USER);
+        dscoin.approve(address(dscengine), amountToMint);
+        dscengine.burnDsc(amountToMint);
+        vm.stopPrank();
+
+        uint256 userBalance = dscoin.balanceOf(USER);
+        assertEq(userBalance, 0);
+    }
 }
---
+++ test/units/DSCoinTest.sol
@@ -0,0 +1,45 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import { DSCoin } from "../../src/DSCoin.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+
+contract DSCoinTest is StdCheats, Test {
+    DSCoin dsc;
+
+    function setUp() public {
+        dsc = new DSCoin();
+    }
+
+    function testMustMintMoreThanZero() public {
+        vm.prank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(this), 0);
+    }
+
+    function testMustBurnMoreThanZero() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(0);
+        vm.stopPrank();
+    }
+
+    function testCantBurnMoreThanYouHave() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(101);
+        vm.stopPrank();
+    }
+
+    function testCantMintToZeroAddress() public {
+        vm.startPrank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(0), 100);
+        vm.stopPrank();
+    }
+}
